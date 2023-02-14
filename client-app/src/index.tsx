8368ceaaf5112218ed1120a551ce6edf91208216
Saikumar kodari <saikdoari@gmail.com>
2023-02-14 13:35:45 -0500

    end of section 5

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './App';
+import './app/layouts/style.css';
+import App from './app/layouts/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
