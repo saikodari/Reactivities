5dedc619f7d9b6f9507e9d7dea83c4bfa06770e9
Saikumar kodari <saikdoari@gmail.com>
2023-03-07 12:29:14 -0500

    end of section 12

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using Application.Activities;
 using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,7 @@ namespace API.Controllers
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return HandleResult(await Mediator.Send(new List.Query()));
-        }
+        }    
         [HttpGet("{id}")]
         public async Task<IActionResult> GetActivity(Guid id)
         {
