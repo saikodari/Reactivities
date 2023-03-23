cabfb2b135f09f67efb9727d696b45d34537082b
Saikumar kodari <saikdoari@gmail.com>
2023-03-23 11:52:01 -0400

    end of section 16

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -13,10 +13,14 @@ namespace Application.Core
             .ForMember(d=>d.HostUsername,o=>o.MapFrom(s=>s.Attendees
             .FirstOrDefault(x=>x.IsHost).AppUser.UserName));
 
-            CreateMap<ActivityAttendee,profiles.Profile>()
+            CreateMap<ActivityAttendee,AttendeeDto>()
             .ForMember(d=>d.DisplayName,o=>o.MapFrom(s=>s.AppUser.DisplayName))
             .ForMember(d=>d.Username,o=>o.MapFrom(s=>s.AppUser.UserName))
-            .ForMember(d=>d.Bio,o=>o.MapFrom(s=>s.AppUser.Bio));
+            .ForMember(d=>d.Bio,o=>o.MapFrom(s=>s.AppUser.Bio))
+            .ForMember(d=>d.Image,o=>o.MapFrom(s=>s.AppUser.Photos.FirstOrDefault(x=>x.IsMain).Url));
+
+            CreateMap<AppUser,profiles.Profile>()
+              .ForMember(d=>d.Image,o=>o.MapFrom(s=>s.Photos.FirstOrDefault(x=>x.IsMain).Url));
         }
     }
 }
